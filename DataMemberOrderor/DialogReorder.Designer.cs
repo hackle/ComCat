b43f823063fc54fc24cb514bea978658c09619f9
unknown <hwayne@TMWS300.TradeMe.local>
2015-06-29 09:38:10 +1200

    Add dialog for ordering

---
+++ DataMemberOrderor/DialogReorder.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DataMemberOrderor
+{
+    partial class DialogReorder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewOrders = new System.Windows.Forms.DataGridView();
+            this.buttonMoveUp = new System.Windows.Forms.Button();
+            this.buttonMoveDown = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewOrders
+            // 
+            this.dataGridViewOrders.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrders.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewOrders.Name = "dataGridViewOrders";
+            this.dataGridViewOrders.Size = new System.Drawing.Size(344, 300);
+            this.dataGridViewOrders.TabIndex = 0;
+            // 
+            // buttonMoveUp
+            // 
+            this.buttonMoveUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonMoveUp.Location = new System.Drawing.Point(376, 12);
+            this.buttonMoveUp.Name = "buttonMoveUp";
+            this.buttonMoveUp.Size = new System.Drawing.Size(75, 23);
+            this.buttonMoveUp.TabIndex = 1;
+            this.buttonMoveUp.Text = "Up";
+            this.buttonMoveUp.UseVisualStyleBackColor = true;
+            this.buttonMoveUp.Click += new System.EventHandler(this.buttonMoveUp_Click);
+            // 
+            // buttonMoveDown
+            // 
+            this.buttonMoveDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonMoveDown.Location = new System.Drawing.Point(376, 41);
+            this.buttonMoveDown.Name = "buttonMoveDown";
+            this.buttonMoveDown.Size = new System.Drawing.Size(75, 23);
+            this.buttonMoveDown.TabIndex = 1;
+            this.buttonMoveDown.Text = "Down";
+            this.buttonMoveDown.UseVisualStyleBackColor = true;
+            this.buttonMoveDown.Click += new System.EventHandler(this.buttonMoveDown_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(376, 97);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // DialogReorder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 328);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonMoveDown);
+            this.Controls.Add(this.buttonMoveUp);
+            this.Controls.Add(this.dataGridViewOrders);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "DialogReorder";
+            this.Text = "Order attributes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewOrders;
+        private System.Windows.Forms.Button buttonMoveUp;
+        private System.Windows.Forms.Button buttonMoveDown;
+        private System.Windows.Forms.Button buttonOK;
+    }
+}
---
+++ DataMemberOrderor/DialogReorder.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DataMemberOrderor
+{
+    using System.Linq;
+
+    using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+    public partial class DialogReorder : Form
+    {
+        private readonly IAttribute[] attributes;
+        private List<AttributeInOrder> listOfOrder;
+
+        IAttribute[] AttributesInOrder
+        {
+            get
+            {
+                return listOfOrder.Select(o => o.Attribute).ToArray();
+            }
+        }
+
+        public DialogReorder(IAttribute[] attributes)
+        {
+            this.attributes = attributes;
+
+            InitializeComponent();
+
+            listOfOrder = new List<AttributeInOrder>();
+            for (var i = 0; i < attributes.Length; i++)
+            {
+                listOfOrder.Add(new AttributeInOrder(attributes[i]) { Order = i });
+            }
+
+            this.dataGridViewOrders.DataSource = listOfOrder;
+        }
+
+        private void MoveUp()
+        {
+            var selectedAttribute = this.GetSelectedAttributeInOrder();
+
+            var index = selectedAttribute.Order;
+            if (index == 0) return;
+
+            this.MoveOrderUp(index);
+        }
+
+        private void MoveDown()
+        {
+            var selectedAttribute = this.GetSelectedAttributeInOrder();
+
+            var index = selectedAttribute.Order;
+            if (index == listOfOrder.Max(o => o.Order)) return;
+
+            this.MoveOrderUp(index + 1);
+        }
+
+        private void MoveOrderUp(int index)
+        {
+            this.MoveBy(index, -1);
+            this.MoveBy(index - 1, 1);
+
+            this.ReBind();
+        }
+
+        private void ReBind()
+        {
+            listOfOrder = listOfOrder.OrderBy(o => o.Order).ToList();
+            this.dataGridViewOrders.DataSource = listOfOrder;
+        }
+
+        private void MoveBy(int order, int offset)
+        {
+            this.listOfOrder.Single(o => o.Order == order).Order += offset;
+        }
+
+        private AttributeInOrder GetSelectedAttributeInOrder()
+        {
+            if (dataGridViewOrders.SelectedRows.Count < 1) return null;
+
+            return dataGridViewOrders.SelectedRows[0].DataBoundItem as AttributeInOrder;
+        }
+
+        private void buttonMoveUp_Click(object sender, System.EventArgs e)
+        {
+            this.MoveUp();
+        }
+
+        private void buttonMoveDown_Click(object sender, System.EventArgs e)
+        {
+            this.MoveDown();
+        }
+    }
+
+    public class AttributeInOrder
+    {
+        public IAttribute Attribute { get; set; }
+
+        public AttributeInOrder(IAttribute attribute)
+        {
+            this.Attribute = attribute;
+        }
+
+        public int Order { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return this.Attribute.Name.QualifiedName;
+            }
+        }
+    }
+}
