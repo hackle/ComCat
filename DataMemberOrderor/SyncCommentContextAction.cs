397ae13bd2119cfb80c591fcb342cc8921fcb937
Hackle Wayne <hacklew@hotmail.com>
2015-07-04 16:16:24 +1200

    Add support for recursive type recognition

--- DataMemberOrderor/SyncCommentContextAction.cs
+++ DataMemberOrderor/SyncCommentContextAction.cs
@@ -6,8 +6,10 @@ using JetBrains.Application.Progress;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Bulbs;
 using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
+using JetBrains.ReSharper.Feature.Services.CSharp.CompleteStatement;
 using JetBrains.ReSharper.Intentions.Extensibility;
 using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp.Parsing;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.ExtensionsAPI.Tree;
 using JetBrains.ReSharper.Psi.Tree;
@@ -17,17 +19,15 @@ using JetBrains.Util;
 namespace DataMemberOrderor
 {
     [ContextAction(Name = "Use base type comment", Group = "C#", Description = "Use comment from base type.", Priority = -20)]
-    public class SyncCommentActionBase : ContextActionBase
+    public class SyncCommentContextAction : ContextActionBase
     {
         protected readonly ICSharpContextActionDataProvider Provider;
 
         private const string UsedKeyWord = "base type comment";
         
-        private IExpression _selectedExpression;
+        private ITreeNode _currentNode;
 
-        private IDocCommentNode _oldCommentNode;
-
-        public SyncCommentActionBase(ICSharpContextActionDataProvider provider)
+        public SyncCommentContextAction(ICSharpContextActionDataProvider provider)
         {
             this.Provider = provider;
         }
@@ -44,29 +44,150 @@ namespace DataMemberOrderor
         {
             using (ReadLockCookie.Create())
             {
-                this._oldCommentNode = this.Provider.TokenBeforeCaret as IDocCommentNode;
-                return null != this._oldCommentNode;
+                this._currentNode = Provider.SelectedElement;
+
+                return NodeIsMethodComment(this._currentNode) || NodeIsImplementingClass();
             }
+        }
+
+        private bool NodeIsImplementingClass()
+        {
+            return NodeIsClass() && ClassHasInheritingMethods(GetParentClass(this._currentNode));
+        }
+
+        private bool NodeIsClass()
+        {
+            var classDeclaration = GetParentClass(this._currentNode);
+
+            return null != classDeclaration;
+        }
+
+        private bool ClassHasInheritingMethods(IClassDeclaration classDeclaration)
+        {
+            return GetMethodInheritance(classDeclaration).Any();
+        }
+
+        private IEnumerable<MethodInheritance> GetMethodInheritance(IClassDeclaration classDeclaration)
+        {
+            var publicMethods =
+                classDeclaration.MethodDeclarations.Where(IsVirtualOrAbstractMethod).ToArray();
+
+            if (!publicMethods.Any()) return new MethodInheritance[0];
+
+            var superTypes = GetSupertypesRecursive(classDeclaration);
+
+            var baseMethods = superTypes.SelectMany(GetTypeMethods);
+
+            return from baseMethod in baseMethods
+                join localMethod in publicMethods
+                    on baseMethod.DeclaredElement.ToString() equals localMethod.DeclaredElement.ToString()
+                select new MethodInheritance()
+                {
+                    BaseMethod = baseMethod,
+                    ChildMethod = localMethod
+                };
+        }
+
+        private IEnumerable<IClassLikeDeclaration> GetSupertypesRecursive(IClassLikeDeclaration classDeclaration)
+        {
+            var superTypes = new List<IClassLikeDeclaration>();
+
+            superTypes.AddRange(classDeclaration.SuperTypes.SelectMany(t => t.GetTypeElement().GetDeclarations().Select(d => d as IClassLikeDeclaration)));
+
+            if (superTypes.Any())
+            {
+                superTypes.AddRange(superTypes.SelectMany(GetSupertypesRecursive));
+            }
+
+            return superTypes;
+        }
+
+        private IEnumerable<IMethodDeclaration> GetTypeMethods(IClassLikeDeclaration superType)
+        {
+            if (superType is IInterfaceDeclaration)
+                return this.GetMethods(superType as IInterfaceDeclaration);
+
+            return this.GetMethods(superType as IClassDeclaration);
+        }
+
+        private IClassDeclaration GetParentClass(ITreeNode treeNode)
+        {
+            return treeNode.Parent as IClassDeclaration;
+        }
+
+        private bool NodeIsMethodComment(ITreeNode currentNode)
+        {
+            var commentNode = currentNode as IDocCommentNode;
+            if (null == commentNode) return false;
+
+            return IsMethodDocComment(commentNode);
+        }
+
+        private static bool IsMethodDocComment(ITreeNode commentNode)
+        {
+            return commentNode.Parent is IDocCommentBlockNode && commentNode.Parent.Parent is IMethodDeclaration;
         }
 
         protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
         {
-            if (null == this._oldCommentNode)
+            if (null == this._currentNode)
             {
                 return null;
             }
 
-            ITreeNode currentNode = this._oldCommentNode.Parent as IDocCommentBlockNode;
-
-            var methodDeclaration = currentNode.Parent as IMethodDeclaration;
-            if (null == methodDeclaration)
+            if (NodeIsMethodComment(this._currentNode))
             {
-                return null;
-            }
+                ITreeNode currentNode = this._currentNode.Parent as IDocCommentBlockNode;
 
-            this.TryUseBaseTypeComments(methodDeclaration);
+                var methodDeclaration = currentNode.Parent as IMethodDeclaration;
+                if (null == methodDeclaration)
+                {
+                    return null;
+                }
+
+                this.TryUseBaseTypeComments(methodDeclaration);
+            }
+            else
+            {
+                var methodInheritances = GetMethodInheritance(GetParentClass(this._currentNode));
+
+                foreach (var methodPair in methodInheritances)
+                {
+                    ReplaceOrInsertComments(methodPair.BaseMethod, methodPair.ChildMethod);
+                }
+            }
 
             return null;
+        }
+
+        private void ReplaceOrInsertComments(IMethodDeclaration baseMethod, IMethodDeclaration childMethod)
+        {
+            var baseComments = GetMethodComments(baseMethod);
+
+            if (null == baseComments) return;
+
+            var childComments = GetMethodComments(childMethod);
+
+            if (null == childComments)
+            {
+                InsertComments(childMethod, baseComments);
+            }
+            else
+            {
+                var replacement = baseComments.Copy();
+
+                ModificationUtil.ReplaceChild(childComments, baseComments);
+            }
+        }
+
+        private void InsertComments(IMethodDeclaration method, IDocCommentBlockNode comments)
+        {
+            ModificationUtil.AddChildBefore(method.FirstChild, comments);
+        }
+
+        private IDocCommentBlockNode GetMethodComments(IMethodDeclaration baseMethod)
+        {
+            return baseMethod.Children().SingleOrDefault(n => n is IDocCommentBlockNode) as IDocCommentBlockNode;
         }
 
         private void TryUseBaseTypeComments(IMethodDeclaration methodDeclaration)
@@ -92,7 +213,7 @@ namespace DataMemberOrderor
 
             IMethodDeclaration matchingMehtod =
                 methodDeclarations.FirstOrDefault(
-                    m => m.DeclaredElement.ToString() == methodDeclaration.DeclaredElement.ToString());
+                    m => MethodsAreEqual(m, methodDeclaration));
 
             if (null == matchingMehtod)
             {
@@ -106,6 +227,11 @@ namespace DataMemberOrderor
             {
                 ModificationUtil.ReplaceChild(originalNode, targetNode);
             }
+        }
+
+        bool MethodsAreEqual(IMethodDeclaration declaration1, IMethodDeclaration declaration2)
+        {
+            return declaration1.DeclaredElement.ToString() == declaration2.DeclaredElement.ToString();
         }
 
         private IEnumerable<IMethodDeclaration> GetMethods(IInterfaceDeclaration interfaceDeclaration)
@@ -125,7 +251,19 @@ namespace DataMemberOrderor
                 return new IMethodDeclaration[0];
             }
 
-            return classDeclaration.MethodDeclarations;
+            return classDeclaration.MethodDeclarations.Where(IsVirtualOrAbstractMethod);
         }
+
+        private bool IsVirtualOrAbstractMethod(IMethodDeclaration methodDeclaration)
+        {
+            return methodDeclaration.ModifiersList.HasModifier(CSharpTokenType.VIRTUAL_KEYWORD) ||
+                   methodDeclaration.ModifiersList.HasModifier(CSharpTokenType.ABSTRACT_KEYWORD);
+        }
+    }
+
+    class MethodInheritance
+    {
+        public IMethodDeclaration BaseMethod { get; set; }
+        public IMethodDeclaration ChildMethod { get; set; }
     }
 }
